474b7b1a40ab2d1ce6542b5ebcaa252e130fd660
Harward, Nathan A <nathan.a.harward@intel.com>
2017-10-30 19:28:13 -0700

    added tmux files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 nateharward's dotfiles
+github.com/nateharward
+
+contains:
+* VIM .vimrc
+* TMUX .tmux.conf
--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
 ln -sv ${BASEDIR}/vimrc ~/.vimrc
+
+# tmux 
+ln -sv ${BASEDIR}/tmux.conf ~/.tmux.conf
